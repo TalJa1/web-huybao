e7e85cea3960dc1c572e59b871f0a369ea0883b2
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-10-30 09:52:07 +0700

    Refactor Home view to use TopLayout component and add Main subcomponent

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import TopLayout from "../components/TopLayout";
+import { Box } from "@mui/material";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <TopLayout>
+      <Main />
+    </TopLayout>
+  );
+};
+
+const Main: React.FC = () => {
+  return <Box>Home</Box>;
 };
 
 export default Home;
