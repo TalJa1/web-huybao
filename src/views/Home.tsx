9e1286adb8fc21fe3c7fc2f5f6c8f194de45488d
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-10-30 11:28:07 +0700

    Enhance Home component layout with responsive grid structure and add student/teacher statistics with custom SVG icons

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -3,6 +3,7 @@ import TopLayout from "../components/TopLayout";
 import { Box, InputAdornment, TextField } from "@mui/material";
 import Grid from "@mui/material/Grid2";
 import SearchIcon from "@mui/icons-material/Search";
+import { studentSVG, teacherIcon } from "../assets/iconSVG";
 
 const Home = () => {
   return (
@@ -16,7 +17,7 @@ const Main: React.FC = () => {
   return (
     <Box>
       <Grid container spacing={2}>
-        <Grid size={9} container>
+        <Grid size={{md: 9, xs: 12}} container>
           <Grid size={12} container sx={{ alignItems: "center" }}>
             <Grid
               size={8}
@@ -44,18 +45,84 @@ const Main: React.FC = () => {
             </Grid>
           </Grid>
           <Grid size={12} container>
-            <Grid size={4} sx={{ color: "#000000", fontSize: "24px" }}>
-              Thống kê
+            <Grid size={{ md: 4, xs: 12 }} container alignItems="center">
+              <Grid size={3}>
+                <Box
+                  sx={{
+                    backgroundColor: "#1568B1",
+                    borderRadius: "50%",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    width: 50,
+                    height: 50,
+                  }}
+                >
+                  {studentSVG({ width: 30, height: 30, color: "white" })}
+                </Box>
+              </Grid>
+              <Grid size={9}>
+                <Box sx={{ display: "flex", flexDirection: "column" }}>
+                  <Box sx={{ color: "#818181", fontSize: 18 }}>Học sinh</Box>
+                  <Box sx={{ fontSize: 36, fontWeight: "700", color: "black" }}>
+                    100
+                  </Box>
+                </Box>
+              </Grid>
             </Grid>
-            <Grid size={4} sx={{ color: "#000000", fontSize: "24px" }}>
-              Thống kê
+            <Grid size={{ md: 4, xs: 12 }} container alignItems="center">
+              <Grid size={3}>
+                <Box
+                  sx={{
+                    backgroundColor: "#EE7223",
+                    borderRadius: "50%",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    width: 50,
+                    height: 50,
+                  }}
+                >
+                  {teacherIcon({ width: 30, height: 30, color: "white" })}
+                </Box>
+              </Grid>
+              <Grid size={9}>
+                <Box sx={{ display: "flex", flexDirection: "column" }}>
+                  <Box sx={{ color: "#818181", fontSize: 18 }}>Giáo viên</Box>
+                  <Box sx={{ fontSize: 36, fontWeight: "700", color: "black" }}>
+                    234
+                  </Box>
+                </Box>
+              </Grid>
             </Grid>
-            <Grid size={4} sx={{ color: "#000000", fontSize: "24px" }}>
-              Thống kê
+            <Grid size={{ md: 4, xs: 12 }} container alignItems="center">
+              <Grid size={3}>
+                <Box
+                  sx={{
+                    backgroundColor: "#1568B1",
+                    borderRadius: "50%",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    width: 50,
+                    height: 50,
+                  }}
+                >
+                  {studentSVG({ width: 30, height: 30, color: "white" })}
+                </Box>
+              </Grid>
+              <Grid size={9}>
+                <Box sx={{ display: "flex", flexDirection: "column" }}>
+                  <Box sx={{ color: "#818181", fontSize: 18 }}>Phí ăn</Box>
+                  <Box sx={{ fontSize: 36, fontWeight: "700", color: "black" }}>
+                    đ32.000.000
+                  </Box>
+                </Box>
+              </Grid>
             </Grid>
           </Grid>
         </Grid>
-        <Grid size={3}></Grid>
+        <Grid size={{md: 3, xs: 12}}></Grid>
       </Grid>
     </Box>
   );
