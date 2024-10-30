20a877eaec1120db0f231d5baaa9338b516fce79
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-10-30 17:58:29 +0700

    Add header section to FinancePage with icons and user info

--- src/views/FinancePage.tsx
+++ src/views/FinancePage.tsx
@@ -1,6 +1,8 @@
 import { Box } from "@mui/material";
 import React from "react";
+import Grid from "@mui/material/Grid2";
 import TopLayout from "../components/TopLayout";
+import { bellIcon, settingIcon } from "../assets/iconSVG";
 
 const FinancePage = () => {
   return (
@@ -11,7 +13,47 @@ const FinancePage = () => {
 };
 
 const Main: React.FC = () => {
-  return <Box sx={{ padding: "20px" }}></Box>;
+  return (
+    <Box sx={{ padding: "20px" }}>
+      <Grid container>
+        <Grid size={8}>
+          <Box sx={{ color: "#000000", fontSize: "36px", fontWeight: "700" }}>
+            Tài chính
+          </Box>
+        </Grid>
+        <Grid size={4}>
+          <Header />
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+const Header: React.FC = () => {
+  return (
+    <Grid container size={12} display={"flex"} alignItems={"center"}>
+      <Grid size={2}>{bellIcon({})}</Grid>
+      <Grid size={2}>{settingIcon({})}</Grid>
+      <Grid size={8} container>
+        <Grid
+          size={6}
+          display="flex"
+          flexDirection="column"
+          alignItems={"flex-end"}
+        >
+          <Grid sx={{ color: "#000000" }}>Thu Hiền</Grid>
+          <Grid sx={{ color: "#818181" }}>Admin</Grid>
+        </Grid>
+        <Grid size={6} display={"flex"} justifyContent={"center"}>
+          <img
+            style={{ width: "50px", height: "50px" }}
+            alt=""
+            src={require("../assets/home/avatar.png")}
+          />
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default FinancePage;
