500297f7e2383a5caa16e4c4564e127d2bc1e6b6
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-10-30 14:24:22 +0700

    Add Main section to HomeRightComponent with activity overview and action button

--- src/components/home/HomeRightComponent.tsx
+++ src/components/home/HomeRightComponent.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import Grid from "@mui/material/Grid2";
 import React from "react";
 import { bellIcon, settingIcon } from "../../assets/iconSVG";
@@ -7,6 +7,52 @@ const HomeRightComponent = () => {
   return (
     <Box sx={{ padding: "20px" }}>
       <Header />
+      <Main />
+    </Box>
+  );
+};
+
+const Main: React.FC = () => {
+  return (
+    <Box>
+      <Grid container sx={{ margin: "10px 0" }}>
+        <Grid container size={10}>
+          <Grid
+            size={12}
+            sx={{ color: "black", fontSize: "24", fontWeight: "700" }}
+          >
+            Đang hoạt động
+          </Grid>
+          <Grid
+            sx={{ color: "#818181", fontSize: "14", fontWeight: "400" }}
+            size={12}
+          >
+            456 học sinh
+          </Grid>
+        </Grid>
+        <Grid
+          size={2}
+          display={"flex"}
+          justifyContent={"center"}
+          alignItems={"center"}
+        >
+          <Button
+            sx={{
+              backgroundColor: "#1568B1",
+              color: "white",
+              borderRadius: "50%",
+              width: "40px",
+              height: "40px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              minWidth: "40px",
+            }}
+          >
+            +
+          </Button>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
