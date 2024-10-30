336d9a38f0416cc46181f30b62ae41b3e0b1cf8e
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-10-30 14:14:45 +0700

    Add SVG icons and integrate them into HomeRightComponent for enhanced UI

--- src/components/home/HomeRightComponent.tsx
+++ src/components/home/HomeRightComponent.tsx
@@ -1,7 +1,41 @@
+import { Box } from "@mui/material";
+import Grid from "@mui/material/Grid2";
 import React from "react";
+import { bellIcon, settingIcon } from "../../assets/iconSVG";
 
 const HomeRightComponent = () => {
-  return <div>HomeRightComponent</div>;
+  return (
+    <Box sx={{ padding: "20px" }}>
+      <Header />
+    </Box>
+  );
+};
+
+const Header: React.FC = () => {
+  return (
+    <Grid container size={12} display={"flex"} alignItems={"center"}>
+      <Grid size={2}>{bellIcon({})}</Grid>
+      <Grid size={2}>{settingIcon({})}</Grid>
+      <Grid size={8} container>
+        <Grid
+          size={6}
+          display="flex"
+          flexDirection="column"
+          alignItems={"flex-end"}
+        >
+          <Grid sx={{ color: "#000000" }}>Thu Hiền</Grid>
+          <Grid sx={{ color: "#818181" }}>Admin</Grid>
+        </Grid>
+        <Grid size={6} display={"flex"} justifyContent={"center"}>
+          <img
+            style={{ width: "50px", height: "50px" }}
+            alt=""
+            src={require("../../assets/home/avatar.png")}
+          />
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default HomeRightComponent;
