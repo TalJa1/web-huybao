b6a025466f5d359a8ccdb942059579f1cc95728f
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-10-30 16:17:57 +0700

    Add TeacherPage component and route to App

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router";
 import { BrowserRouter } from "react-router-dom";
 import Home from "./views/Home";
 import StudentPage from "./views/StudentPage";
+import TeacherPage from "./views/TeacherPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/students" element={<StudentPage />} />
+        <Route path="/teachers" element={<TeacherPage />} />
       </Routes>
     </BrowserRouter>
   );
