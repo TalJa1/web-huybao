6def73492232fc9d2828542db22e7fb8aef6e0c9
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-10-31 09:28:05 +0700

    Implement pagination and meal display in MealPage component

--- src/views/MealPage.tsx
+++ src/views/MealPage.tsx
@@ -1,8 +1,16 @@
-import { Box } from "@mui/material";
-import React from "react";
+import { Box, Chip, Pagination, Typography } from "@mui/material";
+import React, { useState } from "react";
 import Grid from "@mui/material/Grid2";
 import TopLayout from "../components/TopLayout";
-import { bellIcon, settingIcon } from "../assets/iconSVG";
+import {
+  bellIcon,
+  chartIcon,
+  likeIcon,
+  settingIcon,
+  starIcon,
+  threeDotsIcon,
+} from "../assets/iconSVG";
+import { MealData } from "../services/renderData";
 
 const MealPage = () => {
   return (
@@ -13,6 +21,20 @@ const MealPage = () => {
 };
 
 const Main: React.FC = () => {
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const mealsPerPage = 5;
+
+  const handlePageChange = (
+    event: React.ChangeEvent<unknown>,
+    value: number
+  ) => {
+    setCurrentPage(value);
+  };
+
+  const indexOfLastMeal = currentPage * mealsPerPage;
+  const indexOfFirstMeal = indexOfLastMeal - mealsPerPage;
+  const currentMeals = MealData.slice(indexOfFirstMeal, indexOfLastMeal);
+
   return (
     <Box sx={{ padding: "20px" }}>
       <Grid container>
@@ -26,6 +48,95 @@ const Main: React.FC = () => {
         </Grid>
       </Grid>
       {/* Render here */}
+      <Box sx={{ marginTop: "20px" }}>
+        {currentMeals.map((meal, index) => (
+          <Grid
+            container
+            key={index}
+            sx={{
+              marginBottom: 2,
+              padding: 2,
+              alignItems: "center",
+              backgroundColor: "white",
+              borderRadius: "10px",
+              boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
+            }}
+          >
+            <Grid size={1} display="flex" justifyContent="center">
+              <Box
+                sx={{
+                  width: 50,
+                  height: 50,
+                  backgroundColor: "#25ABE1",
+                  borderRadius: "10px",
+                }}
+              />
+            </Grid>
+            <Grid size={2}>
+              <Typography>
+                <Chip
+                  label={meal.catergory}
+                  sx={{ fontSize: "14px" }}
+                  color="primary"
+                />
+              </Typography>
+              <Box sx={{ fontSize: "24px", fontWeight: "700", color: "black" }}>
+                {meal.name}
+              </Box>
+            </Grid>
+            <Grid size={2} display="flex" alignItems="center">
+              {starIcon({})}
+              <Box
+                sx={{
+                  marginLeft: 1,
+                  fontSize: "24px",
+                  fontWeight: "700",
+                  color: "black",
+                }}
+              >
+                {meal.star}
+              </Box>
+            </Grid>
+            <Grid size={2} display="flex" alignItems="center">
+              {chartIcon({})}
+              <Box
+                sx={{
+                  marginLeft: 1,
+                  fontSize: "24px",
+                  fontWeight: "700",
+                  color: "black",
+                }}
+              >
+                {meal.order}
+              </Box>
+            </Grid>
+            <Grid size={2} display="flex" alignItems="center">
+              {likeIcon({})}
+              <Typography
+                sx={{
+                  marginLeft: 1,
+                  fontSize: "24px",
+                  fontWeight: "700",
+                  color: "black",
+                }}
+              >
+                {meal.like}%
+              </Typography>
+            </Grid>
+            <Grid size={1} display="flex" justifyContent="flex-end">
+              {threeDotsIcon({})}
+            </Grid>
+          </Grid>
+        ))}
+      </Box>
+      <Box display="flex" justifyContent="center" sx={{ marginTop: 2 }}>
+        <Pagination
+          count={Math.ceil(MealData.length / mealsPerPage)}
+          page={currentPage}
+          onChange={handlePageChange}
+          color="primary"
+        />
+      </Box>
     </Box>
   );
 };
